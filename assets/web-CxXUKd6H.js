d5667a70485664414dfcf93cbc40f3ebe8748f62
satyam-devx <satyam-devx@users.noreply.github.com>
2026-09-07 05:30:04 +0000

    deploy: 7d9dc3897f580f0bb1201c2c64e84536ebc4b222 — Enhance responsiveness and animation timing in SplashScreen
    Updated the SplashScreen component to improve responsiveness and animation timing. Adjusted delays and added comments for clarity. 7d9dc3897f580f0bb1201c2c64e84536ebc4b222

--- assets/web-CxXUKd6H.js
+++
@@ -1,1 +0,0 @@
-import{bt as e}from"./index-Bc2D83Hj.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
---
+++ assets/web-D1NDzxyP.js
@@ -0,0 +1,1 @@
+import{bt as e}from"./index-CWLNABby.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
